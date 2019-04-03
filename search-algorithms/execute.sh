4273038f8a0f9edd0d239b677fccc765e43d3503
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-04-03 06:57:31 -0300

    Remove duplicate welcome

--- search-algorithms/execute.sh
+++ search-algorithms/execute.sh
@@ -4,5 +4,4 @@ cd skyscrapers-puzzle/
 mvn clean compile assembly:single
 cd target
 clear
-echo WELCOME TO SKYSCRAPER PUZZLE SOLVER
 java -jar skyscrapers-puzzle-1.0-SNAPSHOT-jar-with-dependencies.jar

5bc9feb6245aaab998df33d5dc8a0f926ca58870
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-05-04 16:05:09 -0300

    Save info from terrain05.data into a matrix & add test

---
+++ neural-networks/tests/terrain-test.m
@@ -0,0 +1,6 @@
+1 #not a fucntion
+addpath('./terrains')
+addpath('./utility_functions')
+
+load_file('terrain05.data');
+
---
+++ neural-networks/utility_functions/load_files.m
@@ -0,0 +1,20 @@
+function info = load_file(terrain)
+    data = dlmread(terrain,'',1,0);
+    
+    length = length(data / 3);
+    x = data(1:length);
+    y = data(length+1: length*2);
+    z = data(length*2 + 1: length*3);
+
+    # Info format
+    # [x1 y1]   z1 (k = 1)
+    # [x2 y2]   z2 (k= 2)
+    # . . . 
+    # [xn yn]   zn (k=n)
+    info = {};
+    for k = [1:length]
+        info{k}{1} = [x(k) ; y(k)];
+        info{k}{2} = z(k);
+    end
+    info # --> Uncomment if running terrain-test.m
+endfunction

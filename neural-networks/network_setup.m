4b8e0f68c927cc5bc5b0f5d8fb6fd8281c41ce58
Mica Banfi <mbanfi@itba.edu.ar>
2019-05-09 11:19:35 -0300

    mini changes

--- neural-networks/network_setup.m
+++ neural-networks/network_setup.m
@@ -2,10 +2,12 @@ function out = network_setup(terrain_file, max_epochs, sample_number)
     global arq;
     global W;
     global g;
+    global eta;
     full_patterns = load_file(terrain_file);
     normalized_patterns = normalize(full_patterns);
     test_patterns = randomize_patterns(normalized_patterns, sample_number);
-    out = learn(test_patterns, max_epochs, false, random_pass = false, momentum = 0.9, aep = [], with_error = true);
+    out = learn(W,test_patterns, g,eta,max_epochs, false, random_pass = false, momentum = 0.9, aep = [], with_error = true);
+    
     result = out{1};
     plot_nn(result, full_patterns, g); 
 endfunction
--- neural-networks/script.m
+++ neural-networks/script.m
@@ -5,12 +5,12 @@ addpath('./utility_functions')
 addpath('./terrains')
 
 terrain_file = './terrains/terrain05.data';
-max_epochs = 50;
+max_epochs = 100;
 sample_number = 200;
 global arq = [2 10 5 1];
 global eta = 0.03;
 global W = generate_weights_random(arq);
 global g = {{@tanh, @dtanh}, {@tanh, @dtanh}, {@(x) x, @(x) 1}};
 
-network_setup(terrain, max_epochs, sample_number);
+network_setup(terrain_file, max_epochs, sample_number);
 

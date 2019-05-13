b617a59077a084f7812dd4d8d00aac02dfe31f3b
Mica Banfi <mbanfi@itba.edu.ar>
2019-05-13 08:22:32 -0300

    real time

--- neural-networks/plot/plot_error.m
+++ neural-networks/plot/plot_error.m
@@ -3,7 +3,6 @@ function plot_error(error_array)
     x=[1:1:cant];
     graphics_toolkit('gnuplot');
     figure(2)
-    refresh(2)
     plot(x,error_array)
     title("Grafico del error")
     ylabel("Error")
